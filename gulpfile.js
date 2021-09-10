d7d9ff153d24ef57c92e6f691e4b8f209aa2c944
Anton <antonhogstrom1994@gmail.com>
2021-09-10 13:17:07 +0200

    changed img folder to IMG

--- gulpfile.js
+++ gulpfile.js
@@ -52,14 +52,14 @@ function cssTask() {
 function imgTask() {
     return src(files.imgPath)
     .pipe(imageMin())
-    .pipe(dest('pub/img'))
+    .pipe(dest('pub/IMG'))
 }
 
 //WEBP-task, returns images, optimizes, sends to pub/img/webp
 function webpTask() {
     return src(files.imgPath)
     .pipe(webp())
-    .pipe(dest('pub/img/webp'));
+    .pipe(dest('pub/IMG/webp'));
 }
 
 //Watch-task, Initiates browserSync on pub folder. Watch file-paths seperately, reload on update.
