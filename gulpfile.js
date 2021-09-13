e6460add5b82a5f55517c3811d6de88bbc6b2ad4
Anton <antonhogstrom1994@gmail.com>
2021-09-13 21:28:40 +0200

    Replaced css with sass, changed some code accordingly, uninstalled css-only packages

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,21 @@
 const {src, dest, watch, series, parallel} = require('gulp');
 const concat = require('gulp-concat');
 const terser = require('gulp-terser');
-const postCss = require('gulp-postcss');
-const cssNano = require('cssnano');
-const autoPrefixer = require('autoprefixer');
 const htmlMin = require('gulp-htmlmin');
 const browserSync = require('browser-sync').create();
 const sourceMap = require('gulp-sourcemaps');
 const imageMin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const sass = require('gulp-sass')(require('sass'));
 
-//src directories, targets all html, all css in css folder, all js in js folder and everything in img folder.
+//src directories, targets all html, all css in scss folder, all js in js folder and everything in img folder.
 //img folder should only contain images...
 const files = {
     htmlPath: "src/**/*.html",
     cssPath: "src/css/*.css",
     jsPath: "src/js/*.js",
-    imgPath: "src/IMG/*"
+    imgPath: "src/IMG/*",
+    sassPath: "src/sass/_style.scss"
 }
 
 //HTML-task, returns HTML-files from files.htmlPath and copy them over to destination pub (folder)
@@ -37,15 +36,15 @@ function jsTask() {
     .pipe(dest('pub/js'));
 }
 
-//CSS-task, initiates sourcemap, contatinate, autoprefixing(vendor), minify CSS-files, writes out sourcemap file, push files to pub (folder), activate browserSync stream to make sure CSS updates loads
-function cssTask() {
-    return src(files.cssPath)
-    .pipe(sourceMap.init())
+//SASS-task, returns the main sass-file, initiates sourcemap, cancatinates(for rename), compiles sass to css, compress, log errors, write sourcemap, send file to pub/css, update browser on css changes. 
+function sassTask() {
+    return src(files.sassPath)
+        .pipe(sourceMap.init())
         .pipe(concat('main.css'))
-        .pipe(postCss([ autoPrefixer(), cssNano() ]))
-    .pipe(sourceMap.write('../maps'))
-    .pipe(dest('pub/css'))
-    .pipe(browserSync.stream());
+        .pipe(sass({outputStyle: 'compressed'}).on("error", sass.logError))
+        .pipe(sourceMap.write('../maps'))
+        .pipe(dest("pub/css"))
+        .pipe(browserSync.stream());
 }
 
 //IMG-task, returns images, optimizes, sends to pub/img.
@@ -72,7 +71,7 @@ function watchTask() {
 
     watch(files.jsPath, jsTask).on('change', browserSync.reload);
     watch(files.htmlPath, copyHTML).on('change', browserSync.reload);
-    watch(files.cssPath, cssTask).on('change', browserSync.reload);
+    watch(files.sassPath, sassTask).on('change', browserSync.reload);
     watch(files.imgPath, imgTask).on('change', browserSync.reload);
     watch(files.imgPath, webpTask).on('change', browserSync.reload);
 }
@@ -81,6 +80,6 @@ function watchTask() {
 
 //gulp default exports.
 exports.default = series(
-    parallel(copyHTML, jsTask, cssTask, imgTask, webpTask),
+    parallel(copyHTML, jsTask, sassTask, imgTask, webpTask),
     watchTask
 );
